498611229c4518dd587f27596e442d5f48d4581f
hicelus <ls.balboni@outlook.com>
2020-01-16 13:22:36 -0300

    0.2

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function testar(){
     var elemento = document.getElementById("paragrafo");
     elemento.innerHTML="Teste efetuado";
+    confirm("confirma?");
+    prompt("jacaré", "girafa")
 }
+
+
+function testar2(){
+    document.getElementById("paragrafo").innerHTML="Teste efetuado";
+    alert("testado");
+}
+
+document.getElementById("botao").onclick=function(){testar2()};
