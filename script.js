dd67ad8c31f786b3d2390074ceb4737c83e04ece
hicelus <ls.balboni@outlook.com>
2020-01-17 13:32:37 -0300

    0.2.1

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-var botao = document.getElementById("paragrafo");
+var botao = document.getElementById("botao");
+botao.onclick = function(){testar()};
 
-botao.onclick=function(){testar()};
+var texto = document.getElementById("paragrafo");
+texto.ondblclick = function(){teste2click()};
+texto.onmouseover = function(){entra()};
+texto.onmouseout = function(){sai()};
 
 function testar(){
-    document.getElementById("titulo").innerHTML="Testado";
+    document.getElementById("titulo").innerHTML = "1 Testado";
+}
+
+function teste2click(){
+    document.getElementById("titulo").innerHTML = "2 Testado";
+}
+
+function entra(){
+    botao.innerHTML = "TESTE";
+}
+
+function sai(){
+    botao.innerHTML = "Teste";
 }
